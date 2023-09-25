b700a5ee6c255c5a35970b817a782788b7e47b84
Christober <christobersg@gmail.com>
2023-09-25 08:01:02 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/Boy-pic.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Christober S - Cloud and DevOps. This is a photo of Boy using Technology";
 
 const Home = ({ name, title }) => {
   return (
